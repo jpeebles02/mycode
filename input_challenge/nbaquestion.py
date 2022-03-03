d6898dc0c4da2a526bd99d535eeeb41a9dda0563
jpeebles02 <jpeebles02@users.noreply.github.com>
2022-03-03 17:08:06 +0000

    join and split

---
+++ input_challenge/nbaquestion.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+
+   
+    user_input = input("Who is the best team in the NBA:")
+    
+    
+    print("You said the best team is:", user_input)
+    
+main()
